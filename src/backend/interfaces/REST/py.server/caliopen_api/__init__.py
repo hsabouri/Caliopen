5f67ce27ea2f4ca7c2b9b4018afe5c85bb9fd320
Aymeric Barantal <mric@gandi.net>
2017-12-13 12:08:56 +0100

    Use of pyramid-asyncio package to make pyramid asyncio compliant

--- src/backend/interfaces/REST/py.server/caliopen_api/__init__.py
+++ src/backend/interfaces/REST/py.server/caliopen_api/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.7.0'
-
-
 import logging
 from pyramid.config import Configurator
 from caliopen_storage.config import Configuration
+
+__version__ = '0.7.0'
 
 log = logging.getLogger(__name__)
 
@@ -22,7 +21,9 @@ def main(global_config, **settings):
     caliopen_config = settings['caliopen.config'].split(':')[1]
     Configuration.load(caliopen_config, 'global')
 
-    settings['pyramid_swagger.exclude_paths'] = [r'^/api-ui/?', r'^/doc/api/?', r'^/defs/?']
+    settings['pyramid_swagger.exclude_paths'] = [r'^/api-ui/?',
+                                                 r'^/doc/api/?',
+                                                 r'^/defs/?']
     settings['pyramid_swagger.enable_response_validation'] = True
     config = Configurator(settings=settings)
     services = config.registry.settings. \
@@ -33,4 +34,5 @@ def main(global_config, **settings):
         log.info('Loading %s service' % service)
         config.include(service, route_prefix=route_prefix)
     config.end()
-    return config.make_wsgi_app()
+    config.scan()
+    return config.make_asyncio_app()
