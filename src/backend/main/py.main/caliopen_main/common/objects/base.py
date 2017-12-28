62813761d46bdc04b21bc7c403d46b0cdcc1b49c
Aymeric Barantal <mric@gandi.net>
2017-12-28 16:52:53 +0100

    Important change: ObjectUser take an user object not user_id

--- src/backend/main/py.main/caliopen_main/common/objects/base.py
+++ src/backend/main/py.main/caliopen_main/common/objects/base.py
@@ -286,8 +286,9 @@ class ObjectStorable(ObjectJsonDictifiable):
 class ObjectUser(ObjectStorable):
     """Objects that MUST belong to a user to survive in Caliopen's world..."""
 
-    def __init__(self, user_id=None, **params):
-        self.user_id = user_id
+    def __init__(self, user=None, **params):
+        self.user = user
+        self.user_id = user.user_id
         super(ObjectUser, self).__init__(**params)
 
     @classmethod
@@ -477,7 +478,7 @@ class ObjectIndexable(ObjectUser):
 
         obj_id = getattr(self, self._pkey_name)
         try:
-            self._index = self._index_class.get(index=self.user_id,
+            self._index = self._index_class.get(index=self.user.shard_id,
                                                 id=obj_id,
                                                 using=self._index_class.client())
         except Exception as exc:
@@ -552,19 +553,19 @@ class ObjectIndexable(ObjectUser):
             update = True
         # index_sibling is instanciated with self._index values to perform
         # object comparaison
-        index_sibling = self.__class__(user_id=self.user_id)
+        index_sibling = self.__class__(user=self.user)
         index_sibling._index = self._index
 
         index_sibling.unmarshall_index()
         if not isinstance(self._index, self._index_class):
             self._index = self._index_class()
-            self._index.meta.index = self.user_id
+            self._index.meta.index = self.user.shard_id
             self._index.meta.using = self._index.client()
             self._index.meta.id = getattr(self, self._pkey_name)
 
         # update_sibling is an empty sibling that will be filled
         # with attributes from self
-        update_sibling = self.__class__(user_id=self.user_id)
+        update_sibling = self.__class__(user=self.user)
 
         m = self._index._doc_type.mapping.to_dict()
         for att in m[self._index._doc_type.name]["properties"]:
@@ -607,7 +608,8 @@ class ObjectIndexable(ObjectUser):
             try:
                 self.update_index(wait_for=True)
             except Exception as exc:
-                log.info("apply_patch update_index() exception: {}".format(exc))
+                log.info("apply_patch update_index() exception: {}".
+                         format(exc))
                 raise exc
 
 
